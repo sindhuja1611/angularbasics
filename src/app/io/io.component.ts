712c56983c385c383900294a65ebed2518e6fcc1
sindhuja <sindhuja.s@10decoders.in>
2022-09-09 17:18:54 +0530

    Directives,TODO List,Bootstrap link

--- src/app/io/io.component.ts
+++ src/app/io/io.component.ts
@@ -1,17 +1,37 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-io',
   templateUrl: './io.component.html',
   styleUrls: ['./io.component.css']
 })
-export class IoComponent implements OnInit {
+export class IoComponent {
 
-  @Input() ParentName: any;
+  showusers:Boolean=true;
+  selecteduser:any;
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+ users=[
+  { name:'Sarvesh',Eid:'12',age:35},
+  { name:'Sindhu',Eid:'12',age:35},
+  { name:'Hamrutha',Eid:'12',age:35},
+  { name:'Harsith',Eid:'12',age:35},
+  { name:'Rishi',Eid:'12',age:35}
+ ];
 
+
+ list:any[]=[];
+ addTask(item:string)
+ {
+  this.list.push({id:this.list.length,name:item})
+  console.warn(this.list);
+ }
+removeTask(id:number)
+{
+  console.warn(id);
+  this.list=this.list.filter(item=>item.id! === id);
+
+}
+ 
 }
